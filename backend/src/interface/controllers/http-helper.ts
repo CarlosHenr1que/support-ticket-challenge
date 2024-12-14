101bc603f7611b5778ed35be36a4f69bf979e8de
Carlos Borges <carlosh.mb@outlook.com>
2024-12-14 12:01:32 -0300

    feat: add http helper

---
+++ backend/src/interface/controllers/http-helper.ts
@@ -0,0 +1,21 @@
+import { HttpResponse } from '../adapters/express-route-adapter'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error.message,
+})
+
+export const ok = (data: unknown): HttpResponse => ({
+  statusCode: 200,
+  body: data,
+})
+
+export const created = (data: unknown): HttpResponse => ({
+  statusCode: 201,
+  body: data,
+})
+
+export const serverError = (reason: string): HttpResponse => ({
+  statusCode: 500,
+  body: reason,
+})

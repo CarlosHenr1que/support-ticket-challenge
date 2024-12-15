15c36dfdf3386dcff88d76e84dc13e07fd44dad0
Carlos Borges <carlosh.mb@outlook.com>
2024-12-15 17:15:26 -0300

    feat: add get report to tickets router

--- backend/src/interface/routes/ticket-routes.ts
+++ backend/src/interface/routes/ticket-routes.ts
@@ -8,6 +8,9 @@ import { AddTicket } from '../../use-cases/add-tickets'
 import { TicketMongoRepository } from '../../infra/repositories/ticket-mongo'
 import { UpdateTicket } from '../../use-cases/update-ticket'
 import { adaptRoute } from '../adapters/express-route-adapter'
+import { adaptRouteDownload } from '../adapters/express-donwload-route-adapter'
+import { GetReportController } from '../controllers/get-report'
+import { GenerateTicketReport } from '../../use-cases/generate-report'
 
 const router = Router()
 
@@ -15,13 +18,16 @@ const ticketMongoRepository = new TicketMongoRepository()
 const getTickets = new GetTickets(ticketMongoRepository)
 const addTickets = new AddTicket(ticketMongoRepository)
 const updateTicket = new UpdateTicket(ticketMongoRepository)
+const generateTicketReport = new GenerateTicketReport(ticketMongoRepository)
 
 const getTicketsController = new GetTicketsController(getTickets)
 const addTicketController = new AddTicketController(addTickets)
 const updateTicketController = new UpdateTicketController(updateTicket)
+const getReportController = new GetReportController(generateTicketReport)
 
 router.get('/tickets', adaptRoute(getTicketsController))
 router.post('/tickets', adaptRoute(addTicketController))
 router.put('/tickets/:id', adaptRoute(updateTicketController))
+router.get('/report', adaptRouteDownload(getReportController))
 
 export { router as ticketsRouter }

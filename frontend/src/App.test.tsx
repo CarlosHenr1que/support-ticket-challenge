f9ddf83d6d4b96e7f20b602c9be56f248b99c28a
Carlos Borges <carlosh.mb@outlook.com>
2024-12-16 10:53:09 -0300

    test: ensure tickers are rendered after fetching

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,3 +1,30 @@
-it('Render list of tasks', () => {
-  console.error('You need to add test cases here.');
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './styles/theme';
+
+import * as ticketService from "./api/services/ticket"
+import { Ticket } from './api/services/ticket';
+import { formatDate } from './utils/date';
+
+describe('App Component', () => {
+
+  it('should fetch and display tickets on load', async () => {
+    const tickets: Ticket[] = [{
+      client: "John Doe",
+      issue: "Cannot access my account",
+      status: "open",
+      deadline: "2024-12-15T15:19:14.980Z",
+    }];
+    jest.spyOn(ticketService, "getTicketsRequest").mockResolvedValueOnce(tickets)
+
+    render(<ThemeProvider theme={theme}><App /></ThemeProvider>)
+
+    expect(await screen.findByText(tickets[0].client.toUpperCase())).toBeInTheDocument()
+    expect(await screen.findByText(tickets[0].issue)).toBeInTheDocument()
+    expect(await screen.findByText(formatDate(tickets[0].deadline))).toBeInTheDocument()
+  });
 });

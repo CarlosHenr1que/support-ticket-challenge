ad1946e4fe142d277a4685f3691bebe975556761
Carlos Borges <carlosh.mb@outlook.com>
2024-12-16 11:15:20 -0300

    test: ensure onSwitch is called

--- frontend/src/components/TicketCard/index.test.tsx
+++ frontend/src/components/TicketCard/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { TicketCard } from './index';
 import { Ticket } from '../../api/services/ticket';
 import React from 'react';
@@ -20,5 +20,12 @@ describe('TicketCard', () => {
         expect(screen.getByText(mockTicket.client.toUpperCase())).toBeInTheDocument();
         expect(screen.getByText(formatDate(mockTicket.deadline))).toBeInTheDocument();
         expect(screen.getByText(mockTicket.issue)).toBeInTheDocument();
+    });
+    it('should call onSwitch when switch is toggled', () => {
+        render(<TicketCard position={1} ticket={mockTicket} onSwitch={mockOnSwitch} />);
+
+        const switchElement = screen.getByRole('checkbox');
+        fireEvent.click(switchElement);
+        expect(mockOnSwitch).toHaveBeenCalled();
     });
 });
